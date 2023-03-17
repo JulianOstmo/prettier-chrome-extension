1a0a552557108cb2bc88dbaf1de9eb3c577272f0
Julian Ostmo <julian.ostmo@ibm.com>
2023-03-17 18:17:06 +0000

    feat: created project using chrome-extension-cli

--- dist/popup/popup.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(`Message Received: ${request.greeting}`);
-  if (request.greeting === "hello") sendResponse({ farewell: "goodbye" });
-});
---
+++ src/background.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// With background scripts you can communicate with popup
+// and contentScript files.
+// For more information on background script,
+// See https://developer.chrome.com/extensions/background_pages
+/*
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'GREETINGS') {
+    const message = `Hi ${
+      sender.tab ? 'Con' : 'Pop'
+    }, my name is Bac. I am from Background. It's great to hear from you.`;
+
+    // Log message coming from the `request` parameter
+    console.log(request.payload.message);
+    // Send a response message
+    sendResponse({
+      message,
+    });
+  }
+});
+*/
+
+const sendMessage = (data) => {
+  const { selectionText } = data;
+  console.log('Sending message');
+  chrome.runtime.sendMessage({
+    greeting: selectionText,
+  });
+};
+
+chrome.contextMenus.removeAll(function () {
+  chrome.contextMenus.create({
+    id: '1',
+    title: 'Hello Extensions',
+    contexts: ['selection'], // ContextType
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(sendMessage);
